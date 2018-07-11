66f0ef417edfbe6067178f4d940dbf3af7bed361
Dave Howlett <dave_howlett@hotmail.com>
2018-07-11 15:00:28 +0100

    chained functions example

---
+++ SFA.DAS.Functions.Chained/SFA.DAS.Functions.Chained/Function1.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
+
+namespace SFA.DAS.Functions.Chained
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        [return:Queue("function2queue")]
+        public static string Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
+        {
+            log.Info("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = new StreamReader(req.Body).ReadToEnd();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            return name;
+        }
+    }
+}
