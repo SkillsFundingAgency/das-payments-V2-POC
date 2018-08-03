35c16c1db3223273c54de03564ca9f9a0c8b9703
kola-samuel <samuel.dev.dba@gmail.com>
2018-08-03 15:09:55 +0100

    New Client to Manage ActorID Generation

--- SFA.DAS.Payments/SFA.DAS.Payments.Domain/Config/Configuration.cs
+++ SFA.DAS.Payments/SFA.DAS.Payments.Domain/Config/Configuration.cs
@@ -10,8 +10,8 @@ namespace SFA.DAS.Payments.Domain.Config
         public const int AmplifyEarnings = 1; //20
         public const string ActorConnectionString = "fabric:/SFA.DAS.Payments.DataLock/{0}";
 
-        public const string SqlServerConnectionString = "Server=.;Database=SFA.DAS.Payments.POC;Trusted_Connection=False;User ID=SFActor;Password=SFActor";
-        public const string MetricsConnectionString = "Server=.;Database=SFA.DAS.Payments.POC;Trusted_Connection=False;User ID=SFActor;Password=SFActor";
+        public const string SqlServerConnectionString = "Data Source=.;Initial Catalog=SFA.DAS.Payments.Poc.Database;User ID=SFActor;Password=SFActor";
+        public const string MetricsConnectionString = "Data Source=.;Initial Catalog=SFA.DAS.Payments.Poc.Database;User ID=SFActor;Password=SFActor";
         public const string TableStorageServerConnectionString = "UseDevelopmentStorage=true";
     }
 }
---
+++ SFA.DAS.Payments/SFA.DAS.Payments.ServiceFabric.AutoActorIdPocClient/Program.cs
@@ -0,0 +1,45 @@
+﻿using DataLockActor.Interfaces;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Client;
+using Microsoft.ServiceFabric.Actors.Runtime;
+using System;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Payments.ServiceFabric.AutoActorIdPocClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---- Initialising Actors ----------");
+            Console.WriteLine("Enter C for custom Actor ID generation.");
+            var actorIdGenerationType = Console.ReadLine();
+            var continueProcessing = true;
+
+            while (continueProcessing)
+            {
+                try
+                {
+                    for (long i = 131776765932985901; i < 131776765932985910; i++)
+                    {
+                        var actorId = actorIdGenerationType == "C" ? new ActorId(i.ToString()) : (ActorId.CreateRandom());
+                        var proxy = ActorProxy.Create<IDataLockActor>(actorId, new Uri("fabric:/SFA.DAS.Payments.DataLock/DataLockActor"));
+                        var actorService= proxy.GetActorReference();
+                        Console.WriteLine(actorService.ActorId.GetPartitionKey());
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                Console.WriteLine("----Processing completed - Enter Y to Continue Processing ----------");
+
+                var line = Console.ReadLine();
+                continueProcessing = line == "Y";
+            }
+
+
+        }
+    }
+}
